0a35df2e6cf5a1f8881e7ba7cb8d441b05bf6ddc
Arjeta Avllaj <arjeta.avllaj14@gmail.com>
2018-08-02 16:34:25 +0200

    Add unit test functions.

--- module/Application/test/Controller/IndexControllerTest.php
+++ module/Application/test/Controller/IndexControllerTest.php
@@ -30,6 +30,9 @@ class IndexControllerTest extends AbstractHttpControllerTestCase
         parent::setUp();
     }
 
+    /**
+     * @throws \Exception
+     */
     public function testIndexActionCanBeAccessed()
     {
         $this->dispatch('/', 'GET');
@@ -40,18 +43,49 @@ class IndexControllerTest extends AbstractHttpControllerTestCase
         $this->assertMatchedRouteName('home');
     }
 
+    /**
+     * @throws \Exception
+     */
     public function testIndexActionViewModelTemplateRenderedWithinLayout()
     {
         $this->dispatch('/', 'GET');
         $this->assertQuery('.container form');
+        $this->assertQueryContentRegex('.container form label', '/First name/i');
     }
 
+    /**
+     * @throws \Exception
+     */
     public function testInvalidRouteDoesNotCrash()
     {
         $this->dispatch('/invalid/route', 'GET');
         $this->assertResponseStatusCode(404);
     }
 
+    /**
+     * @throws \Exception
+     */
+    public function testFormInputs()
+    {
+        $this->dispatch('/', 'GET');
+        $this->assertQuery('.container form input[name="first_name"]');
+        $this->assertQuery('.container form input[name="last_name"]');
+        $this->assertQuery('.container form input[name="email_address"]');
+        $this->assertQuery('.container form input[id="first_name"]');
+        $this->assertQuery('.container form input[id="last_name"]');
+        $this->assertQuery('.container form input[id="email_address"]');
+        $this->assertQuery('.container form input[class*="form-control"][id="first_name"]');
+        $this->assertQuery('.container form input[class*="form-control"][id="last_name"]');
+        $this->assertQuery('.container form input[class*="form-control"][id="email_address"]');
+        $this->assertQueryContentRegex('.container form label', '/First name/i');
+        $this->assertQueryContentRegex('.container form label', '/Last name/i');
+        $this->assertQueryContentRegex('.container form label', '/Email Address/i');
+    }
+
+    /**
+     * @throws \Exception
+     * * @var $p $this
+     */
     public function testFormSubmit()
     {
         $p = new Parameters();
@@ -61,9 +95,69 @@ class IndexControllerTest extends AbstractHttpControllerTestCase
         $this->getRequest()->setMethod('POST');
         $this->getRequest()->setPost($p);
         $this->dispatch('/submit');
-//        $this->dispatch('/submit', 'POST', [
-//            'email_address' => "asd@asd.asd",
-//        ]);
+        $this->assertResponseStatusCode(200);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testFormSubmitSuccess()
+    {
+        $this->dispatch('/submit', 'POST', [
+            'email_address' => "asd@asd.asd",
+        ]);
+        $this->assertResponseStatusCode(200);
+        $this->assertQuery('.container .alert');
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testFormSetup()
+    {
+        $this->dispatch('/', 'GET', [
+            'email_address' => 'asd@asd',
+            'first_name' =>'Arjeta',
+        ]);
+        $this->assertQuery('.container .text-danger');
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testEmptyInputFields() {
+
+        $this->dispatch('/submit', 'POST', [
+            'fist_name' => '',
+            'last_name' => '',
+            'email_address' => '',
+        ] );
+         $this->assertResponseStatusCode(200);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testValidInputFields() {
+
+        $this->dispatch('/submit', 'POST', [
+            'fist_name' => 'arjeta',
+            'last_name' => 'avllaj',
+            'email_address' => 'arjeta@test.com',
+        ] );
+        $this->assertResponseStatusCode(200);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testInvalidInputFields() {
+
+        $this->dispatch('/submit', 'POST', [
+            'fist_name' => 'arjeta',
+            'last_name' => '',
+            'email_address' => 'a@',
+        ] );
         $this->assertResponseStatusCode(200);
     }
 }
